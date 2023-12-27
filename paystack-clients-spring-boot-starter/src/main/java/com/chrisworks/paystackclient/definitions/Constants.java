fbdbfdbc024062c13e13e2db3a3a783f5bb79f6f
Christopher Eteka <christopher.eteka@nl.abnamro.com>
2023-12-27 19:02:03 +0100

    Cleanup client's spring boot starter

--- paystack-clients-spring-boot-starter/src/main/java/com/chrisworks/paystackclient/definitions/Constants.java
+++ paystack-clients-spring-boot-starter/src/main/java/com/chrisworks/paystackclient/definitions/Constants.java
@@ -8,4 +8,5 @@ public final class Constants {
     public static final String APPLE_PAY_CLIENT = "apple-pay-client";
     public static final String TRANSACTION_CLIENT = "transaction-client";
     public static final String PRODUCT_CLIENT = "product-client";
+    public static final String SUB_ACCOUNT_CLIENT = "sub-account-client";
 }
